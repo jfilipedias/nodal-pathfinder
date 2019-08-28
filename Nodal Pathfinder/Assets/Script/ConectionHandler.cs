5a15cc6de882f59a626f0136109edcf433df2b23
Filipe Dias <filipediascontato@gmail.com>
2019-08-27 23:36:11 -0300

    Start node prefab

---
+++ Nodal Pathfinder/Assets/Script/ConectionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ConectionHandler : MonoBehaviour
+{
+    public Material materialEnabled, materialConected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Boundary")){
+            this.GetComponent<Renderer>().material = materialConected;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+                this.GetComponent<Renderer>().material = materialEnabled;
+        }
+    }
+}
---
+++ Nodal Pathfinder/Assets/Script/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private int id;
+    private Node boundaryA, boundaryB, boundaryC, bounderyD;
+
+    private void OnEnable()
+    {
+        
+    }
+}
