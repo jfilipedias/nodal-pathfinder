3f1640123d1e2e5c1c09a24164853ff32b71f285
Filipe Dias <filipediascontato@gmail.com>
2019-08-28 23:22:03 -0300

    Start setting up node connections

--- Nodal Pathfinder/Assets/Script/ConectionHandler.cs
+++ Nodal Pathfinder/Assets/Script/ConectionHandler.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+
 public class ConectionHandler : MonoBehaviour
 {
     public Material materialEnabled, materialConected;
+    private Node node;
+
+    private void Awake()
+    {
+        node = this.GetComponentInParent<Node>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Boundary")){
             this.GetComponent<Renderer>().material = materialConected;
+            SetNodeConection(other.transform.GetComponentInParent<Node>());
         }
     }
 
@@ -18,7 +25,45 @@ public class ConectionHandler : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Boundary"))
         {
-                this.GetComponent<Renderer>().material = materialEnabled;
+            this.GetComponent<Renderer>().material = materialEnabled;
+            ClearNodeConection(other.transform.GetComponentInParent<Node>());
+        }
+    }
+
+    private void SetNodeConection(Node nodeConected)
+    {
+        switch (this.name){
+            case "PF_boundary_a":
+                this.node.BoundaryA = nodeConected;
+                break;
+            case "PF_boundary_b":
+                this.node.BoundaryB = nodeConected;
+                break;
+            case "PF_boundary_c":
+                this.node.BoundaryC = nodeConected;
+                break;
+            case "PF_boundary_d":
+                this.node.BoundaryD = nodeConected;
+                break;
+        }
+    }
+
+    private void ClearNodeConection(Node nodeConected)
+    {
+        switch (this.name)
+        {
+            case "PF_boundary_a":
+                this.node.BoundaryA = null;
+                break;
+            case "PF_boundary_b":
+                this.node.BoundaryB = null;
+                break;
+            case "PF_boundary_c":
+                this.node.BoundaryC = null;
+                break;
+            case "PF_boundary_d":
+                this.node.BoundaryD = null;
+                break;
         }
     }
 }
--- Nodal Pathfinder/Assets/Script/Node.cs
+++ Nodal Pathfinder/Assets/Script/Node.cs
@@ -6,9 +6,8 @@ public class Node : MonoBehaviour
 {
     private int id;
     private Node boundaryA, boundaryB, boundaryC, bounderyD;
-
-    private void OnEnable()
-    {
-        
-    }
+    public Node BoundaryA { get; set; }
+    public Node BoundaryB { get; set; }
+    public Node BoundaryC { get; set; }
+    public Node BoundaryD { get; set; }
 }
